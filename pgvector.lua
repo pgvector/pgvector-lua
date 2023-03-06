01bf98ff98a8eef1b13ebf8afdf04712d4e3f524
Andrew Kane <andrew@ankane.org>
2023-03-05 16:59:05 -0800

    Convert to number

--- pgvector.lua
+++ pgvector.lua
@@ -7,7 +7,7 @@ end
 function pgvector.deserialize(v)
   local res = {}
   for x in string.gmatch(string.sub(v, 2, -2), "[^,]+") do
-    table.insert(res, x)
+    table.insert(res, tonumber(x))
   end
   return res
 end

20984c10dc49660df92a9f625b0091bc1a948c2f
Andrew Kane <andrew@ankane.org>
2023-03-05 16:47:56 -0800

    Added deserialize function

--- pgvector.lua
+++ pgvector.lua
@@ -4,4 +4,12 @@ function pgvector.serialize(v)
   return "[" .. table.concat(v, ",") .. "]"
 end
 
+function pgvector.deserialize(v)
+  local res = {}
+  for v in string.gmatch(string.sub(value, 2, -2), "[^,]+") do
+    table.insert(res, v)
+  end
+  return res
+end
+
 return pgvector
